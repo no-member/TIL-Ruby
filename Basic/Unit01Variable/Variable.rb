02eeed5758b5b830e8d7ec329e6753ec9ba32315
shin <cu7898@gmail.com>
2020-05-19 08:12:07 +0900

    TIL 기본 정리

---
+++ Basic/Unit01Variable/Variable.rb
@@ -0,0 +1,14 @@
+myLocalString = 'my local variable'
+myLocalInteger = 1
+myLocalBoolean = true
+myLocalNil = nil
+
+$myGlobalString = 'my global variable'
+$myGlobalInteger = 1
+$myLocalBoolean = true
+$my
+
+puts myLocalString.class
+puts myLocalInteger.class
+puts myLocalBoolean.class
+puts myLocalNil.class
