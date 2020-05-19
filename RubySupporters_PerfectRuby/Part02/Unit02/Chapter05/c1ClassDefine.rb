ae3b1aadf51ea4c1b1822164030af21b16638668
shin <cu7898@gmail.com>
2020-05-19 11:20:40 +0900

    TIL 인스턴스 변수 선언 방법

---
+++ RubySupporters_PerfectRuby/Part02/Unit02/Chapter05/c1ClassDefine.rb
@@ -0,0 +1,9 @@
+class MyClass
+  def hello
+    puts 'Hello, My object!'
+  end
+end
+
+my_object = MyClass.new
+my_object.hello
+
---
+++ RubySupporters_PerfectRuby/Part02/Unit02/Chapter05/c2InstanceVariable2.rb
@@ -0,0 +1,8 @@
+class Ruler
+  attr_accessor :length
+end
+
+ruler = Ruler.new
+
+ruler.length = 40
+puts ruler.length
