c5a625941f9fb445ff8c1f7582d9f00dd25be264
shin <cu7898@gmail.com>
2020-05-19 09:52:55 +0900

    TIL 기본 - 변수의 선언

--- Basic/Unit01Variable/Variable.rb
+++ Basic/Unit01Variable/Variable.rb
@@ -6,9 +6,9 @@ myLocalNil = nil
 $myGlobalString = 'my global variable'
 $myGlobalInteger = 1
 $myLocalBoolean = true
-$my
+$myLocalNil = nil
 
-puts myLocalString.class
-puts myLocalInteger.class
-puts myLocalBoolean.class
-puts myLocalNil.class
+MY_CONSTANT_STRING = 'my constant variable'
+MY_CONSTANT_INTEGER = 1
+MY_CONSTANT_BOOLEAN = true
+MY_CONSTANT_NIL = nil
