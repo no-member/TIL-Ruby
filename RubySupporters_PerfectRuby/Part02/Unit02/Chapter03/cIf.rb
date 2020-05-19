845c8a6b82a27c23c5ce2964bd1a616acb20f439
shin <cu7898@gmail.com>
2020-05-19 10:55:04 +0900

    TIL if문

--- RubySupporters_PerfectRuby/Part02/Unit02/Chapter03/cIf.rb
+++ RubySupporters_PerfectRuby/Part02/Unit02/Chapter03/cIf.rb
@@ -1,3 +1,27 @@
 if true
   puts 'Ping'
 end
+
+str = 'Ping'
+
+if str
+  puts 'My name is Pinggu!'
+end
+
+if false
+  puts 'Pig'
+else
+  puts 'Bubu'
+end
+
+n = 2
+
+if n.zero?
+  puts '0이었다.'
+elsif n.even?
+  puts '짝수였다.'
+elsif n.odd?
+  puts '홀수였다.'
+else
+  puts '그런 숫자가 있나?'
+end
