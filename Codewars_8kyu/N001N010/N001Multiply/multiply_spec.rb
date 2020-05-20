24e41fd02aa2c5ac19cdcf2f8a1bf00e8858f359
shin <cu7898@gmail.com>
2020-05-21 08:06:57 +0900

    TestF #1 calculate 테스트

--- Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
+++ Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
@@ -2,12 +2,12 @@ require 'rspec'
 require_relative 'multiply'
 
 describe Multiply do
-  describe 'multiply_test1' do
-    before do
-      multiply = Multiply.new
+  describe '#calculate' do
+    context '1 * 2 return' do
+      subject { described_class.new }
     end
-    it 'should 1 * 2는 2를 반환함' do
-      expect(mulitply.calculate(1, 2)).to eq 2
+    it '2' do
+      expect(subject.calculate(1, 2)).to eq 2
     end
   end
 end
