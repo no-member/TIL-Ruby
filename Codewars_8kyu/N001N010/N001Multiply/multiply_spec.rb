9074d054277728062bf39dcb4ca38ce8f76a2fbd
shin <cu7898@gmail.com>
2020-05-21 08:10:46 +0900

    Refactor #1

--- Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
+++ Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
@@ -4,9 +4,9 @@ require_relative 'multiply'
 describe Multiply do
   describe '#calculate' do
     context '1 * 2' do
-    end
-    it 'return 2' do
-      expect(subject.calculate(1, 2)).to eq 2
+      it 'return 2' do
+        expect(subject.calculate(1, 2)).to eq 2
+      end
     end
   end
 end
