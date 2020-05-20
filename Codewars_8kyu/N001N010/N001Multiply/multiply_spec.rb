8c76e087391950040b467b938b90adb12741f130
shin <cu7898@gmail.com>
2020-05-21 08:13:10 +0900

    TestP #1 calculate 테스트 케이스 추가

--- Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
+++ Codewars_8kyu/N001N010/N001Multiply/multiply_spec.rb
@@ -5,8 +5,15 @@ describe Multiply do
   describe '#calculate' do
     context '1 * 2' do
       it 'return 2' do
-        expect(subject.calculate(1, 2)).to eq 2
+        expect(subject.calculate(1, 2)).to eql 2
       end
+    end
+
+    context '30 * 3' do
+      it 'return 90' do
+        expect(subject.calculate(30, 3)).to eql 90
+      end
+
     end
   end
 end
