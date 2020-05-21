271562a364456551c622672bef752d2f45d97b2e
shin <cu7898@gmail.com>
2020-05-21 11:23:00 +0900

    TestP #3 테스트 케이스 추가

--- Codewars_8kyu/N001N010/N002EvenOrOdd/number_spec.rb
+++ Codewars_8kyu/N001N010/N002EvenOrOdd/number_spec.rb
@@ -10,10 +10,26 @@ describe Number do
         expect(subject.even_or_odd(2)).to eql("Even");
       end
     end
+    context '0' do
+      it 'should return "Even"' do
+        expect(subject.even_or_odd(0)).to eql("Even");
+      end
+    end
+    context '-8' do
+      it 'should return "Even"' do
+        expect(subject.even_or_odd(-8)).to eql("Even");
+      end
+    end
 
     context '1' do
       it 'should return "Odd"' do
         expect(subject.even_or_odd(1)).to eql("Odd");
+      end
+    end
+
+    context '-1' do
+      it 'should return "Odd"' do
+        expect(subject.even_or_odd(-1)).to eql("Odd");
       end
     end
   end
