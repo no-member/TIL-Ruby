5960468a33dfbe5d7047fd2428e8a56d0cee4b70
shin <cu7898@gmail.com>
2020-05-22 09:55:22 +0900

    TestP #9 make_negative 테스트 - 0일 경우

--- Codewars_8kyu/N001N010/N005ReturnNegative/make_negative_spec.rb
+++ Codewars_8kyu/N001N010/N005ReturnNegative/make_negative_spec.rb
@@ -5,14 +5,24 @@ RSpec.describe MakeNegative do
   subject { described_class.new }
 
   describe '#make_negative' do
-    context 'over 0 test' do
-      it '1 return -1' do
-        expect(subject.make_negative(1)).to eql(-1)
+    describe 'over 0 test' do
+      context '1' do
+        it 'return -1' do
+          expect(subject.make_negative(1)).to eql(-1)
+        end
+      end
+      context '99' do
+        it 'return -99' do
+          expect(subject.make_negative(99)).to eql(-99)
+        end
       end
     end
-    context 'over 0 test' do
-      it '99 return -99' do
-        expect(subject.make_negative(99)).to eql(-99l)
+
+    describe '0 test' do
+      context '0' do
+        it 'return 0' do
+          expect(subject.make_negative(0)).to eql(0)
+        end
       end
     end
   end
