442afa8155ebd86607d5dfa811cb7315e65b10fe
shin <cu7898@gmail.com>
2020-05-22 09:25:02 +0900

    Final #7 최종 정리

--- Codewars_8kyu/N001N010/N004SumOfPositive/sum.rb
+++ Codewars_8kyu/N001N010/N004SumOfPositive/sum.rb
@@ -1,9 +1,15 @@
 class Sum
+  # def positive_sum(arr)
+  #   @sum = 0
+  #   arr.each do |number|
+  #     @sum += number if number > 0
+  #   end
+  #   @sum
+  # end
+
   def positive_sum(arr)
-    @sum = 0
-    arr.each do |number|
-      @sum += number if number > 0
-    end
-    @sum
+    # arr.select { |number| number.positive? }.reduce(0, :+)
+    # arr.select(&:positive?).reduce(0, :+)
+    arr.select(&:positive?).sum
   end
 end
