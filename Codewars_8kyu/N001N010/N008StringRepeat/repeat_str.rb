d4d001e5f7e3661c191b6f9ec8bf3774d0ebf79e
shin <cu7898@gmail.com>
2020-05-25 19:44:46 +0900

    Refactor #15 로직의 간략화

--- Codewars_8kyu/N001N010/N008StringRepeat/repeat_str.rb
+++ Codewars_8kyu/N001N010/N008StringRepeat/repeat_str.rb
@@ -1,13 +1,17 @@
 class RepeatStr
+  # def repeat_str(repeat_number, str)
+  #   @answer = ''
+  #
+  #   i = 0
+  #   while i < repeat_number
+  #     @answer += str
+  #     i += 1
+  #   end
+  #
+  #   @answer
+  # end
+
   def repeat_str(repeat_number, str)
-    @answer = ''
-
-    i = 0
-    while i < repeat_number
-      @answer += str
-      i += 1
-    end
-
-    @answer
+    str * repeat_number
   end
 end
