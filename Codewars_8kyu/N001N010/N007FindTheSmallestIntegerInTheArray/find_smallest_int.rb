5ad1b554ab21c3e01efc7eb60bda0213e80229b3
shin <cu7898@gmail.com>
2020-05-25 19:16:02 +0900

    Refactor #13 더욱 간단한 방법 추가

--- Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int.rb
+++ Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int.rb
@@ -1,9 +1,13 @@
 class FindSmallestInt
+  # def find_smallest_int(arr)
+  #   @smallest = arr[0]
+  #   arr.each do |num|
+  #     @smallest = num if @smallest > num
+  #   end
+  #   @smallest
+  # end
+
   def find_smallest_int(arr)
-    @smallest = arr[0]
-    arr.each do |num|
-      @smallest = num if @smallest > num
-    end
-    @smallest
+    arr.min
   end
 end
