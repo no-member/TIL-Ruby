66654bbf0106c85129f50b970478c94788e76f01
shin <cu7898@gmail.com>
2020-05-25 19:34:04 +0900

    Feat #15 repeat_number 테스트

--- Codewars_8kyu/N001N010/N008StringRepeat/repeat_str.rb
+++ Codewars_8kyu/N001N010/N008StringRepeat/repeat_str.rb
@@ -1,5 +1,9 @@
 class RepeatStr
-  def repeat_str(repeatNumber, str)
-
+  def repeat_str(repeat_number, str)
+    @answer = ''
+    for num in 1..repeat_number do
+      @answer += str
+    end
+    @answer
   end
 end
