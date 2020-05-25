616d850191195ce9b8c961be59b43ca7914289e9
shin <cu7898@gmail.com>
2020-05-25 19:13:32 +0900

    TestP #13 find_smallest_test 테스트 케이스 추가

--- Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int.rb
+++ Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int.rb
@@ -1,6 +1,6 @@
 class FindSmallestInt
   def find_smallest_int(arr)
-    @smallest = arr[0];
+    @smallest = arr[0]
     arr.each do |num|
       @smallest = num if @smallest > num
     end
--- Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int_spec.rb
+++ Codewars_8kyu/N001N010/N007FindTheSmallestIntegerInTheArray/find_smallest_int_spec.rb
@@ -10,5 +10,23 @@ RSpec.describe FindSmallestInt do
         expect(subject.find_smallest_int([72, 2])).to eql(2)
       end
     end
+
+    context '[-8]' do
+      it 'should return -8' do
+        expect(subject.find_smallest_int([-8])).to eql(-8)
+      end
+    end
+
+    context '[1,2,3,4,5,6,7,8,9,10]' do
+      it 'should return 1' do
+        expect(subject.find_smallest_int([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eql(1)
+      end
+    end
+
+    context '[-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]' do
+      it 'should return -10' do
+        expect(subject.find_smallest_int([-1, -2, -3, -4, -5, -6, -7, -8, -9, -10])).to eql(-10)
+      end
+    end
   end
 end
