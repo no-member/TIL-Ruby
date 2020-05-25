0f0524915c211085d21e9b9fd486d45c9b6f7705
shin <cu7898@gmail.com>
2020-05-25 19:40:42 +0900

    TestP #15 repeat_number 테스트 케이스 추가

--- Codewars_8kyu/N001N010/N008StringRepeat/repeat_str_spec.rb
+++ Codewars_8kyu/N001N010/N008StringRepeat/repeat_str_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe RepeatStr do
         expect(subject.repeat_str(3, "*")).to eql("***")
       end
     end
+
+    context '(5, "#")' do
+      it 'should return #####' do
+        expect(subject.repeat_str(5, "#")).to eql("#####")
+      end
+    end
+
+    context '(3, "hi")' do
+      it 'should return hihihi' do
+        expect(subject.repeat_str(3, "hi")).to eql("hihihi")
+      end
+    end
   end
 end
 
