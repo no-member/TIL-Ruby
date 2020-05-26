38d37fedb9e13552606bb89530819f195c1ce1a3
shin <cu7898@gmail.com>
2020-05-26 09:21:49 +0900

    TestP #21 reverse_word 테스트 케이스

--- Codewars_8kyu/N011N020/N011ReverseStrings/reversed_strings_spec.rb
+++ Codewars_8kyu/N011N020/N011ReverseStrings/reversed_strings_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe ReversedStrings do
   subject { described_class.new }
 
   describe '#reverse' do
+    context 'hello' do
+      it 'should return "olleh"' do
+        expect(subject.reverse_word('hello')).to eql('olleh')
+      end
+    end
+
     context 'world' do
       it 'should return "dlrow"' do
         expect(subject.reverse_word('world')).to eql('dlrow')
@@ -14,6 +20,12 @@ RSpec.describe ReversedStrings do
     context 'co' do
       it 'should return "oc"' do
         expect(subject.reverse_word('co')).to eql('oc')
+      end
+    end
+
+    context '' do
+      it 'should return ""' do
+        expect(subject.reverse_word('')).to eql('')
       end
     end
   end
