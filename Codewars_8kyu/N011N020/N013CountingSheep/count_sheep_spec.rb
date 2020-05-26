f1b07e28fac67ddbad5dd4c04168202008dfdd5c
shin <cu7898@gmail.com>
2020-05-26 20:13:33 +0900

    TestP #25 count_sheep 테스트 통과

--- Codewars_8kyu/N011N020/N013CountingSheep/count_sheep_spec.rb
+++ Codewars_8kyu/N011N020/N013CountingSheep/count_sheep_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CountSheep do
   subject { described_class.new }
 
   describe '#count_sheep' do
-    context 'array' do
+    context 'array1' do
       it 'should return 17' do
         array1 = [true, true, true, false,
                   true, true, true, true,
@@ -14,6 +14,18 @@ RSpec.describe CountSheep do
                   true, true, true, true,
                   false, false, true, true]
         expect(subject.count_sheep(array1)).to eql(17)
+      end
+    end
+
+    context 'array2' do
+      it 'should return 18' do
+        array2 = [true, false, true, false,
+                  true, true, true, true,
+                  true, true, true, false,
+                  true, true, false, true,
+                  true, true, true, true,
+                  false, false, true, true]
+        expect(subject.count_sheep(array2)).to eql(18)
       end
     end
   end
