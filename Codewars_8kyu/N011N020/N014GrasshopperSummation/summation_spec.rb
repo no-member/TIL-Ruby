772c0a996bcafe74e260fd33ab6631387ba5d35d
shin <cu7898@gmail.com>
2020-05-27 07:32:26 +0900

    Refactor #27 테스트 정리

--- Codewars_8kyu/N011N020/N014GrasshopperSummation/summation_spec.rb
+++ Codewars_8kyu/N011N020/N014GrasshopperSummation/summation_spec.rb
@@ -5,27 +5,31 @@ RSpec.describe Summation do
   subject { described_class.new }
 
   describe '#summation' do
-    context '2' do
-      it 'should return 3' do
-        expect(subject.summation(2)).to eql(3)
+    describe 'when number is 0 or over 0' do
+      context '2' do
+        it 'should return 3' do
+          expect(subject.summation(2)).to eql(3)
+        end
+      end
+
+      context '3' do
+        it 'should return 6' do
+          expect(subject.summation(3)).to eql(6)
+        end
+      end
+
+      context '0' do
+        it 'should return 0' do
+          expect(subject.summation(0)).to eql(0)
+        end
       end
     end
 
-    context '3' do
-      it 'should return 6' do
-        expect(subject.summation(3)).to eql(6)
-      end
-    end
-
-    context '0' do
-      it 'should return 0' do
-        expect(subject.summation(0)).to eql(0)
-      end
-    end
-
-    context '-10' do
-      it 'should return -45' do
-        expect(subject.summation(-10)).to eql(-45)
+    describe 'when number is under 0' do
+      context '-10' do
+        it 'should return -45' do
+          expect(subject.summation(-10)).to eql(-45)
+        end
       end
     end
   end
