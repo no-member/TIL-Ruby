a4969519ec8a68a1035d81665e923a29871fc21f
shin <cu7898@gmail.com>
2020-05-26 09:34:59 +0900

    TestP #23 테스트 케이스 추가

--- Codewars_8kyu/N011N020/N012RemoveStringSpaces/no_space_spec.rb
+++ Codewars_8kyu/N011N020/N012RemoveStringSpaces/no_space_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe NoSpace do
         expect(subject.no_space('hell o')).to eql('hello')
       end
     end
+
+    context '8 j 8   mBliB8g  imjB8B8  jl  B' do
+      it 'should return "8j8mBliB8gimjB8B8jlB"' do
+        expect(subject.no_space('8 j 8   mBliB8g  imjB8B8  jl  B')).to eql('8j8mBliB8gimjB8B8jlB')
+      end
+    end
   end
 end
 
