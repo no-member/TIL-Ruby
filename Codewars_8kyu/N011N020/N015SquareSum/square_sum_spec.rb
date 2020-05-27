199d149ba78e121cda22682b9235e47b274c1119
shin <cu7898@gmail.com>
2020-05-27 09:29:38 +0900

    TestP #29 square_sum 테스트 케이스 추가

--- Codewars_8kyu/N011N020/N015SquareSum/square_sum_spec.rb
+++ Codewars_8kyu/N011N020/N015SquareSum/square_sum_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SquareSum do
   subject { described_class.new }
 
   describe '#square_sum' do
-    describe 'when number is over 0' do
+    describe 'when numbers include over 0' do
       context '[1, 2]' do
         it 'should return 5' do
           expect(subject.square_sum([1, 2])).to eql(5)
@@ -15,6 +15,22 @@ RSpec.describe SquareSum do
       context '[3, 4, 5]' do
         it 'should return 50' do
           expect(subject.square_sum([3, 4, 5])).to eql(50)
+        end
+      end
+    end
+
+    describe 'when numbers include 0, over 0' do
+      context '[0, 3, 4, 5]' do
+        it 'should return 50' do
+          expect(subject.square_sum([0, 3, 4, 5])).to eql(50)
+        end
+      end
+    end
+
+    describe 'when numbers include 0, under 0, over 0' do
+      context '[-1, -2, 0, 1]' do
+        it 'should return 6' do
+          expect(subject.square_sum([-1, -2, 0, 1])).to eql(6)
         end
       end
     end
