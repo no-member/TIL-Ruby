cebfee3a22367f23a014699beb32beccc6e20336
shin <takamizawa@value-it.co.jp>
2020-05-27 19:55:42 +0900

    TestP #33 string_to_number 테스트 통과

--- Codewars_8kyu/N011N020/N017ConvertAStringToANumber/string_to_number_spec.rb
+++ Codewars_8kyu/N011N020/N017ConvertAStringToANumber/string_to_number_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe StringToNumber do
         expect(subject.string_to_number("123")).to eql(123)
       end
     end
+
+    context '"-123"' do
+      it 'should return -123' do
+        expect(subject.string_to_number("-123")).to eql(-123)
+      end
+    end
+
+    context '"0"' do
+      it 'should return 0' do
+        expect(subject.string_to_number("0")).to eql(0)
+      end
+    end
   end
 end
 
