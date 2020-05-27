68562ac1f98ebf118f0e7b4d8c971945583f57f8
shin <cu7898@gmail.com>
2020-05-27 09:21:36 +0900

    TestF #29 square_num 테스트

--- Codewars_8kyu/N011N020/N015SquareSum/square_sum.rb
+++ Codewars_8kyu/N011N020/N015SquareSum/square_sum.rb
@@ -1,1 +1,6 @@
+class SquareSum
 
+  def square_sum(num)
+    # code here
+  end
+end
--- Codewars_8kyu/N011N020/N015SquareSum/square_sum_spec.rb
+++ Codewars_8kyu/N011N020/N015SquareSum/square_sum_spec.rb
@@ -1,1 +1,17 @@
+require 'rspec'
+require_relative 'square_sum'
+
+RSpec.describe SquareSum do
+  subject { described_class.new }
+
+  describe '#square_sum' do
+    describe 'when number is over 0' do
+      context '2' do
+        it 'should return 5' do
+          expect(subject.square_sum(2)).to eql(5)
+        end
+      end
+    end
+  end
+end
 
