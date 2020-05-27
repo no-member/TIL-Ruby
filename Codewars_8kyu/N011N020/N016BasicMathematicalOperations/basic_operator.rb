39358dbfb8181693aa96ea5bd7a5cc16fe085c1d
shin <takamizawa@value-it.co.jp>
2020-05-27 19:22:17 +0900

    Feat #31 basic_operate 테스트 통과

--- Codewars_8kyu/N011N020/N016BasicMathematicalOperations/basic_operator.rb
+++ Codewars_8kyu/N011N020/N016BasicMathematicalOperations/basic_operator.rb
@@ -1,5 +1,10 @@
 class BasicOperator
   def basic_operate(operator, value1, value2)
-    value1 + value2
+    case operator
+    when '+'
+      value1 + value2
+    when '-'
+      value1 - value2
+    end
   end
 end
