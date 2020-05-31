8a70854454ff4e892a5721602c27e7b7f1d0a841
shin <cu7898@gmail.com>
2020-05-31 22:44:53 +0900

    Refactor #43 is_divisible 더욱 간단한 로직 (타인)

--- Codewars_8kyu/N021N030/N022IsNDivisibleByXAndY/divisible.rb
+++ Codewars_8kyu/N021N030/N022IsNDivisibleByXAndY/divisible.rb
@@ -1,5 +1,9 @@
 class Divisible
-  def is_divisible(n,x,y)
-    n % x == 0 && n % y == 0
+  # def is_divisible(n,x,y)
+  #   n % x == 0 && n % y == 0
+  # end
+
+  def is_divisible(n, *divisors)
+    divisors.all? { |div| n % div == 0 }
   end
 end
