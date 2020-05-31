e3687c965534b49c932bf41dde8393ad1e52b989
shin <cu7898@gmail.com>
2020-05-31 21:43:46 +0900

    Refacotr #41 더욱 쉬운 로직으로 변경 (타인 코드)

--- Codewars_8kyu/N021N030/N021ANeedleInTheHaystack/find_needle.rb
+++ Codewars_8kyu/N021N030/N021ANeedleInTheHaystack/find_needle.rb
@@ -1,12 +1,16 @@
 class FindNeedle
+  # def find_needle(haystack)
+  #   @default_message = 'found the needle at position '
+  #   @needle_index = self.find_needle_index(haystack)
+  #
+  #   @default_message + @needle_index.to_s
+  # end
+  #
+  # def find_needle_index(haystack)
+  #   haystack.index('needle')
+  # end
+
   def find_needle(haystack)
-    @default_message = 'found the needle at position '
-    @needle_index = self.find_needle_index(haystack)
-
-    @default_message + @needle_index.to_s
-  end
-
-  def find_needle_index(haystack)
-    haystack.index('needle')
+    "found the needle at position #{haystack.index('needle')}"
   end
 end
