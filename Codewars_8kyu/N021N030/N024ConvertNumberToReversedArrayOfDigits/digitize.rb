1cd95d1f7c545095e88d60586cbd916bb40310cb
shin <cu7898@gmail.com>
2020-06-01 00:04:53 +0900

    Refactor #47 로직 추가

--- Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize.rb
+++ Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize.rb
@@ -2,6 +2,8 @@ class Digitize
   def digitize(number)
     # String(number).split('').reverse.map(&:to_i)
 
-    number.to_s.chars.reverse.map(&:to_i)
+    # number.to_s.chars.reverse.map(&:to_i)
+
+    number.digits
   end
 end
