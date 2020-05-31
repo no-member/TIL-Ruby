867bac2ddd915edce5540da1bce63fd9f121132b
shin <cu7898@gmail.com>
2020-05-31 21:33:34 +0900

    Feat #41 find_needle 테스트 통과

--- Codewars_8kyu/N021N030/N021ANeedleInTheHaystack/find_needle.rb
+++ Codewars_8kyu/N021N030/N021ANeedleInTheHaystack/find_needle.rb
@@ -1,5 +1,19 @@
 class FindNeedle
   def find_needle(haystack)
+    @default_message = 'found the needle at position '
+    @needle_index = self.find_needle_index(haystack)
 
+    @default_message + @needle_index.to_s
+  end
+
+  def find_needle_index(haystack)
+    index_number = 0
+    haystack.each do |element|
+      if element == 'needle'
+        return index_number
+      end
+
+      index_number += 1
+    end
   end
 end
