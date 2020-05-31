37fc550d17fb13082aca44be0a8759f94308dc8e
shin <cu7898@gmail.com>
2020-05-31 23:42:02 +0900

    TestF #47 digitize 테스트

--- Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize_spec.rb
+++ Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe Digitize do
         expect(subject.digitize(35231)).to eql([1,3,2,5,3])
       end
     end
+
+    context '348597' do
+      it 'should return [7,9,5,8,4,3]' do
+        expect(subject.digitize(348597)).to eql([7,9,5,8,4,3])
+      end
+    end
   end
 end
