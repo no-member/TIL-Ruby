b70a321f9aa5056ea8ef13a0d277629e976abf36
shin <cu7898@gmail.com>
2020-06-01 00:05:16 +0900

    Final #47 digitize 최종 정리

--- Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize.rb
+++ Codewars_8kyu/N021N030/N024ConvertNumberToReversedArrayOfDigits/digitize.rb
@@ -1,9 +1,9 @@
 class Digitize
   def digitize(number)
-    # String(number).split('').reverse.map(&:to_i)
+    String(number).split('').reverse.map(&:to_i)
 
     # number.to_s.chars.reverse.map(&:to_i)
 
-    number.digits
+    # number.digits
   end
 end
