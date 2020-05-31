885b0e0780b2fc628bb7bb0562c26ae27194fb50
shin <cu7898@gmail.com>
2020-06-01 00:25:18 +0900

    TestP #49 maps 테스트 통과

--- Codewars_8kyu/N021N030/N025BeginnerLostWithoutAMap/maps_practice_spec.rb
+++ Codewars_8kyu/N021N030/N025BeginnerLostWithoutAMap/maps_practice_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe MapsPractice do
         expect(subject.maps([1, 2, 3])).to eql([2, 4, 6])
       end
     end
+
+    context '[4, 1, 1, 1, 4]' do
+      it 'should return [8, 2, 2, 2, 8]' do
+        expect(subject.maps([4, 1, 1, 1, 4])).to eql([8, 2, 2, 2, 8])
+      end
+    end
   end
 end
 
