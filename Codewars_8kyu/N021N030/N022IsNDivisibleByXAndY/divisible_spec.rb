b0918d3c0a4dc668b5045b212702300c7460ac83
shin <cu7898@gmail.com>
2020-05-31 22:41:10 +0900

    TestP #43 divisible 테스트 케이스 추가

--- Codewars_8kyu/N021N030/N022IsNDivisibleByXAndY/divisible_spec.rb
+++ Codewars_8kyu/N021N030/N022IsNDivisibleByXAndY/divisible_spec.rb
@@ -5,9 +5,31 @@ RSpec.describe Divisible do
   subject { described_class.new }
 
   describe '#divisible' do
-    context '3, 3, 4' do
-      it 'should return false' do
-        expect(subject.is_divisible(3,3,4)).to eql(false)
+    describe 'return true test' do
+      context '12,3,4' do
+        it 'should return true' do
+          expect(subject.is_divisible(12, 3, 4)).to eql(true)
+        end
+      end
+
+      context '48,3,4' do
+        it 'should return true' do
+          expect(subject.is_divisible(12, 3, 4)).to eql(true)
+        end
+      end
+    end
+
+    describe 'return false test' do
+      context '3, 3, 4' do
+        it 'should return false' do
+          expect(subject.is_divisible(3, 3, 4)).to eql(false)
+        end
+      end
+
+      context '8,3,4' do
+        it 'should return false' do
+          expect(subject.is_divisible(8,3,4)).to eql(false)
+        end
       end
     end
   end
