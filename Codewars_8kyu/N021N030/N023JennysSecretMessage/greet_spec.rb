623722dc5c8e41464b03d548817700d6a5f5f705
shin <cu7898@gmail.com>
2020-05-31 23:09:23 +0900

    TestF #45 greet 테스트

--- Codewars_8kyu/N021N030/N023JennysSecretMessage/greet_spec.rb
+++ Codewars_8kyu/N021N030/N023JennysSecretMessage/greet_spec.rb
@@ -5,21 +5,31 @@ RSpec.describe Greet do
   subject { described_class.new }
 
   describe '#greet' do
-    context 'James' do
-      it 'should return Hello, James!' do
-        expect(subject.greet('James')).to eql('Hello, James!')
+    describe 'greet normal person' do
+      context 'James' do
+        it 'should return Hello, James!' do
+          expect(subject.greet('James')).to eql('Hello, James!')
+        end
+      end
+
+      context 'Jane' do
+        it 'should return Hello, Jane!' do
+          expect(subject.greet('Jane')).to eql('Hello, Jane!')
+        end
+      end
+
+      context 'Jim' do
+        it 'should return Hello, Jim!' do
+          expect(subject.greet('Jim')).to eql('Hello, Jim!')
+        end
       end
     end
 
-    context 'Jane' do
-      it 'should return Hello, Jane!' do
-        expect(subject.greet('Jane')).to eql('Hello, Jane!')
-      end
-    end
-
-    context 'Jim' do
-      it 'should return Hello, Jim!' do
-        expect(subject.greet('Jim')).to eql('Hello, Jim!')
+    describe 'greet Jonny with special messgae' do
+      context 'Jonny' do
+        it 'should return Hello, my love!' do
+          expect(subject.greet('Jonny')).to eql('Hello, my love!')
+        end
       end
     end
   end
