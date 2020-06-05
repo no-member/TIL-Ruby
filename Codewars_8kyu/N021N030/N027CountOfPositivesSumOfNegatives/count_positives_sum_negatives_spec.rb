8031ea1098e559467e5a64c30846f770f09f692a
shin <cu7898@gmail.com>
2020-06-05 18:30:48 +0900

    TestF #53 count_positives_sum_negative 테스트 통과

--- Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
+++ Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe CountPositiveSumNegative do
         expect(subject.count_positives_sum_negative([1])).to eql([1, 0])
       end
     end
+
+    context '[1, 2]' do
+      it 'should return [2, 0]' do
+        expect(subject.count_positives_sum_negative([1, 2])).to eql([2, 0])
+      end
+    end
   end
 end
 
