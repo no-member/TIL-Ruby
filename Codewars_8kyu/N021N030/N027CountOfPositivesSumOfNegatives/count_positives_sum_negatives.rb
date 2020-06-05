04b0fbd274207782218cf5589351338c33461944
shin <cu7898@gmail.com>
2020-06-05 18:28:32 +0900

    Feat #53 count_positives_sum_negative 테스트 통과

--- Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives.rb
+++ Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives.rb
@@ -1,1 +1,5 @@
-
+class CountPositiveSumNegative
+  def count_positives_sum_negative (numbers)
+    [1, 0]
+  end
+end
--- Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
+++ Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
@@ -1,1 +1,15 @@
+require 'rspec'
+require_relative 'count_positives_sum_negatives'
+
+RSpec.describe CountPositiveSumNegative do
+  subject { described_class.new }
+
+  describe '#count_positives_sum_negative' do
+    context '[1]' do
+      it 'should return [1, 0]' do
+        expect(subject.count_positives_sum_negative([1])).to eql([1, 0])
+      end
+    end
+  end
+end
 
