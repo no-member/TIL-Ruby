6f07872c21d5f29254808889372d95b6947dc320
shin <cu7898@gmail.com>
2020-06-05 18:40:59 +0900

    Feat #53 count_positives_sum_negative 테스트 통과

--- Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives.rb
+++ Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives.rb
@@ -1,10 +1,14 @@
 class CountPositiveSumNegative
   def count_positives_sum_negative (numbers)
-    [count_positives(numbers), 0]
+    [count_positives(numbers), sum_negative(numbers)]
   end
 
   private
   def count_positives (numbers)
     numbers.select { |n| n.positive? }.length
+  end
+
+  def sum_negative (numbers)
+    numbers.select { |n| n.negative? }.sum
   end
 end
