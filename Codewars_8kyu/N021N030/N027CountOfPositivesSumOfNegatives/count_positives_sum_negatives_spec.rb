9256ec4a1999d036245cd21245a979b13ef6a126
shin <cu7898@gmail.com>
2020-06-05 18:36:05 +0900

    Refactor #53

--- Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
+++ Codewars_8kyu/N021N030/N027CountOfPositivesSumOfNegatives/count_positives_sum_negatives_spec.rb
@@ -5,15 +5,17 @@ RSpec.describe CountPositiveSumNegative do
   subject { described_class.new }
 
   describe '#count_positives_sum_negative' do
-    context '[1]' do
-      it 'should return [1, 0]' do
-        expect(subject.count_positives_sum_negative([1])).to eql([1, 0])
+    describe 'count positives' do
+      context '[1]' do
+        it 'should return [1, 0]' do
+          expect(subject.count_positives_sum_negative([1])).to eql([1, 0])
+        end
       end
-    end
 
-    context '[1, 2]' do
-      it 'should return [2, 0]' do
-        expect(subject.count_positives_sum_negative([1, 2])).to eql([2, 0])
+      context '[1, 2]' do
+        it 'should return [2, 0]' do
+          expect(subject.count_positives_sum_negative([1, 2])).to eql([2, 0])
+        end
       end
     end
   end
