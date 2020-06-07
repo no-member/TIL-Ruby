32be59bb8aac5bedfeed043fd51681444908fd90
shin <cu7898@gmail.com>
2020-06-07 09:15:33 +0900

    TestP #56 count_monkey 테스트

--- Codewars_8kyu/N021N030/N028CountTheMonkeys/monkey_count_spec.rb
+++ Codewars_8kyu/N021N030/N028CountTheMonkeys/monkey_count_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe MonkeyCount do
         expect(subject.monkey_count(2)).to eql([1, 2])
       end
     end
+
+    context '10' do
+      it 'should return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]' do
+        expect(subject.monkey_count(10)).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+      end
+    end
   end
 end
 
