04ca1caf6c93697044161765d6fe01e3f738f7a7
takamizawa <cu7898@gmail.com>
2020-06-08 16:23:33 +0900

    Test #60 double 테스트 통과

--- Codewars_8kyu/N021N030/N030YouCantCodeUnderPressure/double_integer_spec.rb
+++ Codewars_8kyu/N021N030/N030YouCantCodeUnderPressure/double_integer_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe DoubleInteger do
         expect(subject.double(1)).to eql(2)
       end
     end
+
+    context '3' do
+      it 'should return 6' do
+        expect(subject.double(3)).to eql(6)
+      end
+    end
+
+    context '-4' do
+      it 'should return -8' do
+        expect(subject.double(-4)).to eql(-8)
+      end
+    end
   end
 end
 
