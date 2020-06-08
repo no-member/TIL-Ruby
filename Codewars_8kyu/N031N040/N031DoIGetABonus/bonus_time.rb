d9ba995592fbe43f745bca8ac66a6030e0b4819c
takamizawa <cu7898@gmail.com>
2020-06-08 16:48:10 +0900

    TestF #62 bonus_time 테스트

---
+++ Codewars_8kyu/N031N040/N031DoIGetABonus/bonus_time.rb
@@ -0,0 +1,5 @@
+class BonusTime
+  def bonus_time (salary, bonus)
+
+  end
+end
---
+++ Codewars_8kyu/N031N040/N031DoIGetABonus/bonus_time_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative 'bonus_time'
+
+RSpec.describe BonusTime do
+  subject { described_class.new }
+
+  describe '#bonus_time' do
+    context '10000, true' do
+      it 'should return 100000' do
+        expect(subject.bonus_time(10000, true)).to eql('$100000')
+      end
+    end
+  end
+end
