38a1eda15e46fce93bcb6c370f05ee600e6a14e3
shin <cu7898@gmail.com>
2020-07-11 21:14:08 +0900

    TestF +points

--- Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils.rb
+++ Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils.rb
@@ -1,1 +1,19 @@
+class MyNumberUtils
+  def points(games)
+    result = 0
+    games.each { |game|
+      result += judge(game)
+    }
+    result
+  end
 
+  private
+
+  def judge(game)
+    x, y = game.split(":")
+    if x == y
+      return 1
+    end
+    x > y ? 3 : 0
+  end
+end
--- Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils_spec.rb
+++ Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils_spec.rb
@@ -1,31 +1,43 @@
 require 'rspec'
-require_relative ''
+require_relative './my_number_utils'
 
-RSpec.describe MyArrayUtils do
+RSpec.describe MyNumberUtils do
   subject { described_class.new }
 
-  describe '.have_element_in_array?' do
-    context 'with [66, 101], 66' do
-      it "return true" do
-        expect(subject.have_element_in_array?([66, 101], 66)).to eql(true)
+  describe 'points 함수는' do
+    context 'x가 y보다 클 경우가 1개 있을 경우' do
+      it '3을 반환한다.' do
+        expect(subject.points(['3:1'])).to eql(3)
       end
     end
 
-    context 'with [66, 101], 7' do
-      it "return false" do
-        expect(subject.have_element_in_array?([66, 101], 7)).to eql(false)
+    context 'x가 y보다 클 경우가 10개 있을 경우' do
+      it '30을 반환한다.' do
+        expect(subject.points(%w[1:0 2:0 3:0 4:0 2:1 3:1 4:1 3:2 4:2 4:3])).to eql(30)
       end
     end
 
-    context 'with ["t", "e", "s", "t"], "e"' do
-      it "return true" do
-        expect(subject.have_element_in_array?(["t", "e", "s", "t"], "e")).to eql(true)
+    context 'x가 y랑 같은 경우가 1개 있을 경우' do
+      it '1을 반환한다.' do
+        expect(subject.points(['1:1'])).to eql(1)
       end
     end
 
-    context 'with ["t", "e", "s", "t"], "c"' do
-      it "return false" do
-        expect(subject.have_element_in_array?(["t", "e", "s", "t"], "c")).to eql(false)
+    context 'x가 y랑 같은 경우가 10개 있을 경우' do
+      it '10을 반환한다.' do
+        expect(subject.points(%w[1:1 2:2 3:3 4:4 2:2 3:3 4:4 3:3 4:4 4:4])).to eql(10)
+      end
+    end
+
+    context 'x가 y보다 작은 경우가 1개 있을 경우' do
+      it '0을 반환한다.' do
+        expect(subject.points(['1:3'])).to eql(0)
+      end
+    end
+
+    context 'x가 y보다 작은 경우가 10개 있을 경우' do
+      it '0을 반환한다.' do
+        expect(subject.points(%w[0:1 0:2 0:3 0:4 1:2 1:3 1:4 2:3 2:4 3:4])).to eql(0)
       end
     end
   end
