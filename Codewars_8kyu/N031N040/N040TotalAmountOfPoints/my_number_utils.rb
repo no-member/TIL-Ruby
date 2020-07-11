1331d6143e5737ccf5a3dda9162d72f4654004eb
shin <cu7898@gmail.com>
2020-07-11 21:16:02 +0900

    Final +points

--- Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils.rb
+++ Codewars_8kyu/N031N040/N040TotalAmountOfPoints/my_number_utils.rb
@@ -1,19 +1,23 @@
 class MyNumberUtils
+  # def points(games)
+  #   result = 0
+  #   games.each { |game|
+  #     result += judge(game)
+  #   }
+  #   result
+  # end
+  #
+  # private
+  #
+  # def judge(game)
+  #   x, y = game.split(":")
+  #   if x == y
+  #     return 1
+  #   end
+  #   x > y ? 3 : 0
+  # end
+
   def points(games)
-    result = 0
-    games.each { |game|
-      result += judge(game)
-    }
-    result
-  end
-
-  private
-
-  def judge(game)
-    x, y = game.split(":")
-    if x == y
-      return 1
-    end
-    x > y ? 3 : 0
+    games.sum { |score| [1, 3, 0][score[0] <=> score[2]] }
   end
 end
