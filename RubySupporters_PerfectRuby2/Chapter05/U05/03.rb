1d9f85750ba4577c59dbbff7201d1a1aa0ccb255
shin <cu7898@gmail.com>
2020-07-20 08:35:22 +0900

    TIL 해쉬

--- RubySupporters_PerfectRuby2/Chapter05/U05/03.rb
+++ RubySupporters_PerfectRuby2/Chapter05/U05/03.rb
@@ -2,7 +2,7 @@ puts("# hash")
 puts("## 기본 정보")
 
 puts("### 키 처리")
-p array = [ 1, 2 ]
+p array = [1, 2]
 p array.hash
 p array.reverse!
 p array.hash
@@ -45,24 +45,24 @@ puts()
 
 puts("### 특정 값만 남기기")
 p hash = {foo: 1, bar: 2, baz: 3}
-p hash.select {|key, value| value.odd?} # {:foo=>1, :baz=>3}
+p hash.select { |key, value| value.odd? } # {:foo=>1, :baz=>3}
 p hash # {foo: 1, bar: 2, baz: 3}
-p hash.select! {|key, value| value.odd?} # {:foo=>1, :baz=>3}
+p hash.select! { |key, value| value.odd? } # {:foo=>1, :baz=>3}
 p hash # {:foo=>1, :baz=>3}
 p hash = {foo: 1, bar: 2, baz: 3} # {foo: 1, bar: 2, baz: 3}
-p hash.select! {|key, value| true} # nil
-p hash.keep_if {|key, value| true} # {foo: 1, bar: 2, baz: 3}
+p hash.select! { |key, value| true } # nil
+p hash.keep_if { |key, value| true } # {foo: 1, bar: 2, baz: 3}
 puts()
 
 puts("### 특정 값만 삭제하기")
 p hash = {foo: 1, bar: 2, baz: 3}
-p hash.reject {|key, value| value.odd?} # {:bar=>2}
+p hash.reject { |key, value| value.odd? } # {:bar=>2}
 p hash # {foo: 1, bar: 2, baz: 3}
-p hash.reject! {|key, value| value.odd?} # {:bar=>2}
+p hash.reject! { |key, value| value.odd? } # {:bar=>2}
 p hash # {:bar=>2}
 p hash = {foo: 1, bar: 2, baz: 3}
-p hash.reject! {|key, value| false} # nil
-p hash.delete_if {|key, value| false} # {foo: 1, bar: 2, baz: 3}
+p hash.reject! { |key, value| false } # nil
+p hash.delete_if { |key, value| false } # {foo: 1, bar: 2, baz: 3}
 puts
 
 puts("## 병합")
@@ -78,6 +78,7 @@ p a.merge(b) # {:one=>"A", :two=>"B", :three=>"C"}
 puts
 
 puts "### merge 응용"
+
 def keywords(hash = {})
   defaults = {alice: '앨리스', bob: '밥'}
   defaults.merge(hash)
@@ -164,7 +165,7 @@ p has_default['foo'] # "evian"
 puts
 
 puts "### 블록 형태 지정"
-has_default = Hash.new {|hash, key| Time.now}
+has_default = Hash.new { |hash, key| Time.now }
 p has_default['foo'] # 2020-07-16 08:01:43.032874 +0900
 sleep 1
 p has_default['foo'] # 2020-07-16 08:01:44.033604 +0900
@@ -188,7 +189,21 @@ puts
 puts "### fetch"
 hash = {}
 p hash.fetch('foo', 'this is default') # "this is default"
-p hash.fetch('foo') {|key| key} # "foo"
+p hash.fetch('foo') { |key| key } # "foo"
 # p hash.fetch('foo') # `fetch': key not found: "foo" (KeyError)
+puts
 
+puts "## 해시 변환"
+puts "### to_a"
+p a = {one: 1, two: 2} # {:one=>1, :two=>2}
+p a.to_a # [[:one, 1], [:two, 2]]
+p a.assoc(:one) # [:one, 1]
+puts
+
+puts "### Hash.[]"
+p ary = %w(key1 one key2 two)
+p Hash[*ary] # {"key1"=>"one", "key2"=>"two"}
+
+p ary = [%w(key1 one), %w(key2 two)]
+p Hash[ary] # {"key1"=>"one", "key2"=>"two"}
 
