6458fc9137fc685f4ee1e1104f479ca01230fb96
shin <cu7898@gmail.com>
2020-07-31 21:58:52 +0900

    TIL downto

--- Codewars_8kyu/N041N050/N041ReversedSequence/my_number_utils.rb
+++ Codewars_8kyu/N041N050/N041ReversedSequence/my_number_utils.rb
@@ -1,11 +1,14 @@
 class MyNumberUtils
+  # def reverse_seq(num)
+  #   seq(num).reverse
+  # end
+  #
+  # private
+  # def seq(num)
+  #   (1..num).to_a
+  # end
+
   def reverse_seq(num)
-    seq(num).reverse
+    num.downto(1).to_a
   end
-
-  private
-  def seq(num)
-    (1..num).to_a
-  end
-
 end
