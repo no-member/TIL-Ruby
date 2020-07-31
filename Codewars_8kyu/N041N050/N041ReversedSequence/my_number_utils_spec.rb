ef9fef0d0d03623c18d638790c8c57f607869127
shin <cu7898@gmail.com>
2020-07-31 20:57:11 +0900

    N.041 - Reversed sequence #82

--- Codewars_8kyu/N041N050/N041ReversedSequence/my_number_utils_spec.rb
+++ Codewars_8kyu/N041N050/N041ReversedSequence/my_number_utils_spec.rb
@@ -3,10 +3,11 @@ require 'rspec'
 
 RSpec.describe MyNumberUtils do
   subject { described_class.new }
-  describe 'reverse_seq' do
-    context 'when give 1' do
-      it 'return [1]' do
 
+  describe 'reverse_seq' do
+    context 'when give 2' do
+      it 'return [2, 1]' do
+        expect(subject.reverse_seq(2)).to eql([2, 1])
       end
     end
   end
