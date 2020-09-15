d15a8e764810c171c1ff0e7d4a2cf2b87520a4d1
shin <cu7898@gmail.com>
2020-09-15 09:30:01 +0900

    Ultisnips: 예시 추가

---
+++ basic/array/min_max.rb
@@ -0,0 +1,7 @@
+arr = [1, 2, 30, 3, 44, 5]
+p arr.min
+p arr.max
+puts
+
+p arr.min(2)
+p arr.max(2)
---
+++ basic/string/upcase_downcase.rb
@@ -0,0 +1,5 @@
+str_upper = 'hi my uppercase'
+p str_upper.upcase
+
+str_lowercase = 'HI MY LOWERCASE'
+p str_lowercase.downcase
