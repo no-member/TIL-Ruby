d15b57efcc14b6abd1347707d0d16e3d6e7b9449
shin <shin@value-it.co.jp>
2020-10-16 13:36:25 +0900

    Study: p와 puts의 차이

---
+++ books/RubySupporters_PerfectRuby/Chapter03/unit06/section01/s_p.rb
@@ -0,0 +1,19 @@
+class Ruler
+  def initialize(length)
+    @length = length
+  end
+
+  def to_s
+    '=' * @length
+  end
+
+  def inspect
+    "Ruler(length: #{@length})"
+  end
+end
+
+ruler = Ruler.new(30)
+
+puts ruler
+
+p ruler
