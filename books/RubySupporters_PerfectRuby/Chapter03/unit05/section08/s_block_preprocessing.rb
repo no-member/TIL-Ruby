58aaa6d7928331bd6931a0b2885dd9a680550ba1
shin <shin@value-it.co.jp>
2020-10-16 11:04:36 +0900

    Study: 전처리, 후처리에 사용 할 수 있는 블록문

---
+++ books/RubySupporters_PerfectRuby/Chapter03/unit05/section08/s_block_preprocessing.rb
@@ -0,0 +1,14 @@
+def write_with_lock
+  File.open 'time.txt', 'w' do |f|
+    f.flock File::LOCK_EX
+
+    yield f
+
+    f.flock File::LOCK_UN
+  end
+end
+
+write_with_lock do |f|
+  f.puts Time.now
+  f.puts 'Hello, world!'
+end
