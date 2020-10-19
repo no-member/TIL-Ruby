d902c8a9ced2aa9184eb3306dd7ff318ed97274e
shin <shin@value-it.co.jp>
2020-10-19 09:59:17 +0900

    Refactor: 다시 공부를 하기위해 기존 파일 삭제
    もう一度勉強するため既存に存在するファールを削除

--- books/RubySupporters_PerfectRuby/Chapter05/U03/06.rb
+++
@@ -1,9 +0,0 @@
-pattern = /(?<=동)(\d+)(?=번지)/
-
-puts pattern.match('경기도1번지파주시문방동13번지')
-
-pattern2 = /(?<!2012)-(?<month_and_day>\d{2}-\d{2})/
-puts pattern2.match('2013-01-01') # 출력
-puts pattern2.match('2012-01-01') # 출력 안함
-puts pattern2.match('2011-01-01') # 출력
-
