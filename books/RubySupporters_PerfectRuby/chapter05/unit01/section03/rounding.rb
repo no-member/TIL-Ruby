30f2f358e55e41297dfd1410d03874e0bfe576a7
shin <shin@value-it.co.jp>
2020-10-19 10:31:56 +0900

    Study: 라운딩 처리
    * rounding
    * rounding

---
+++ books/RubySupporters_PerfectRuby/chapter05/unit01/section03/rounding.rb
@@ -0,0 +1,26 @@
+p 'round'
+p 1.4.round
+p 1.5.round
+p -1.4.round
+p -1.5.round
+puts
+
+p 'ceil'
+p 1.4.ceil
+p 1.5.ceil
+p -1.4.ceil
+p -1.5.ceil
+puts
+
+p 'floor'
+p 1.4.floor
+p 1.5.floor
+p -1.4.floor
+p -1.5.floor
+puts
+
+p 'truncate'
+p 1.4.truncate
+p 1.5.truncate
+p -1.4.truncate
+p -1.5.truncate
