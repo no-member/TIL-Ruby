f887c20d3be1509597fc7ab777d35485829de80b
shin <shin@value-it.co.jp>
2020-10-19 12:16:58 +0900

    Study: integer에서 사용 할 수 있는 기본 제공 메소드
    * integerで使える基本メソッド

---
+++ books/RubySupporters_PerfectRuby/chapter05/unit01/section05/integer_utils.rb
@@ -0,0 +1,23 @@
+p 'odd와 even'
+p 1.odd?
+p 1.even?
+puts
+
+p 2.odd?
+p 2.even?
+puts
+
+p 'next succ pred'
+p 2.next
+p 2.succ
+p 2.pred
+puts
+
+p 'to_s, to_i'
+p 13.to_s
+p '13'.to_i
+puts
+
+4.times do |num|
+  p num
+end
