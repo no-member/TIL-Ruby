3444dbdd1b3e9033b28d28b64f09627bb3116ebd
Shin <cu7898@gmail.com>
2020-11-03 17:08:14 +0900

    Study: 루비 온 레일즈 5

--- rails/railbook/config/routes.rb
+++ rails/railbook/config/routes.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'hello/index'
 
   get 'hello/view'
 
   get 'hello/list'
+
+  get 'view/keyword'
+
+  post 'keyword/search'
+
+  get 'view/form_tag'
+
+  post 'view/create'
+
+  get 'view/form_for'
+
+  get 'view/field'
+
+  get 'view/html5'
 end
